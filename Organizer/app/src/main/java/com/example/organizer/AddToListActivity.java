beca99d13d60826370a3d4a18be55273c4f5fb4d
katarzynawilczak <kasia.wilczak@yahoo.com>
2019-04-25 19:11:51 +0200

    list update

---
+++ Organizer/app/src/main/java/com/example/organizer/AddToListActivity.java
@@ -0,0 +1,69 @@
+package com.example.organizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class AddToListActivity extends AppCompatActivity {
+
+    private Button addBtn;
+    private EditText nameTxt;
+    private EditText descpTxt;
+    private DatabaseReference listDataBase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_list);
+        listDataBase = FirebaseDatabase.getInstance().getReference().child("List");
+
+        addBtn = (Button) findViewById(R.id.addTaskBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nameTxt = (EditText) findViewById(R.id.txtName);
+                descpTxt = (EditText) findViewById(R.id.txtDescp);
+                //TODO exceptions!
+                String taskName = nameTxt.getText().toString().trim();
+                String taskDescp = descpTxt.getText().toString().trim();
+                //add to db
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name", taskName);
+                dataMap.put("Description", taskDescp);
+                dataMap.put("Status", "Not done");
+                listDataBase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //check if stored correctly
+                        if(task.isSuccessful()) {
+                            Toast.makeText(AddToListActivity.this,  "Added", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(AddToListActivity.this,  "Error", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+                //back to the list
+                Intent intent = new Intent(AddToListActivity.this, ListActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
--- Organizer/app/src/main/java/com/example/organizer/ListActivity.java
+++ Organizer/app/src/main/java/com/example/organizer/ListActivity.java
@@ -1,44 +1,94 @@
 package com.example.organizer;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
-import java.io.FileOutputStream;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
-
-    String [] notesList = {"item1", "item2", "it"};
-
+    private Button addToDbBtn;
+    private DatabaseReference listDataBase;
+    private ListView viewList;
+    private ArrayList<String> keysList = new ArrayList<>();
+    private ArrayList<String> notesList = new ArrayList<>();
+    private ArrayList<String> descpList = new ArrayList<>();
+    private ArrayList<String> statusList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        listDataBase = FirebaseDatabase.getInstance().getReference().child("List");
 
 
-        String fileContents = "Itemno1\nItem 2\nitem33";
-        FileOutputStream outputStream;
+        addToDbBtn = (Button) findViewById(R.id.addToDbBtn);
+        addToDbBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open form
+                Intent intent = new Intent(ListActivity.this, AddToListActivity.class);
+                startActivity(intent);
 
-        try {
-            //File notesFile = getFilesDir();
-            outputStream = openFileOutput("notes", Context.MODE_PRIVATE);
-            outputStream.write(fileContents.getBytes());
-            notesList[0] = outputStream.toString();
-            //notesList = outputStream.toString().split("\\n");
-            outputStream.close();
+            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
 
-        ListAdapter adap = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notesList);
-        ListView viewList = (ListView) findViewById(R.id.viewList);
-        viewList.setAdapter(adap);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notesList);
+        viewList = (ListView) findViewById(R.id.viewList);
+        viewList.setAdapter(adapter);
+
+
+        listDataBase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String id = dataSnapshot.getKey();
+                //DatabaseReference keyRef = FirebaseDatabase.getInstance().getReference().child("List").child(id);
+                keysList.add(id);
+                String nameData = dataSnapshot.child("Name").getValue(String.class);
+                notesList.add(nameData);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.child("Name").getValue(String.class);
+                String key = dataSnapshot.getKey();
+                int index = keysList.indexOf(key);
+                notesList.set(index,value);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
